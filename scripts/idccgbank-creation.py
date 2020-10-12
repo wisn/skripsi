15245461b843471d82e1182989f6589834aef127
Wisnu Adi Nurcahyo <wisn.adn@gmail.com>
2020-10-13 06:44:42 +0700

    Add removeNonProjectives() method

--- scripts/idccgbank-creation.py
+++ scripts/idccgbank-creation.py
@@ -4,12 +4,18 @@ import os.path
 import sys
 # pylint: disable=no-name-in-module
 from ufal.udpipe import Model, Pipeline, ProcessingError
+import conllu
+
+cd = os.path.dirname(os.path.realpath(__file__))
+udModelPath = cd + "/../models/indonesian-gsd-ud-2.5-191206.udpipe"
+udInputPath = cd + "/../datasets/id_pud-ud-test-revised-1-0.conllu"
+udParsedPath = cd + "/../datasets/id_pud-ud-revised-1-0.parsed.conllu"
 
 class IDPUDParser:
-    def __init__(self, udModelPath: str, udInputPath: str, udOutputPath: str):
+    def __init__(self, udModelPath: str, udInputPath: str, udParsedPath: str):
         self.udModelPath = udModelPath
         self.udInputPath = udInputPath
-        self.udOutputPath = udOutputPath
+        self.udParsedPath = udParsedPath
 
         self.errMsg = ""
         self.parsed = False
@@ -18,7 +24,7 @@ class IDPUDParser:
         ret = "IDPUDParser {"
         ret += "\n  udModelPath = \"%s\"," % self.udModelPath
         ret += "\n  udInputPath = \"%s\"," % self.udInputPath
-        ret += "\n  udOutputPath = \"%s\"," % self.udOutputPath
+        ret += "\n  udParsedPath = \"%s\"," % self.udParsedPath
         ret += "\n  parsed = %s," % self.parsed
         ret += "\n}"
         return ret
@@ -47,11 +53,71 @@ class IDPUDParser:
             self.errMsg += err.message
             return
 
-        outFile = open(self.udOutputPath, "w")
+        outFile = open(self.udParsedPath, "w")
         outFile.write(processed)
         outFile.close()
 
         self.parsed = True
+
+class CONLLU:
+    def __init__(self, inputPath: str):
+        self.inputPath = inputPath
+
+        self.tokenList = []
+        self.tokenTree = None
+
+    def __str__(self):
+        ret = "CONLLU {"
+        ret += "\n  inputPath = %s" % self.inputPath
+        if len(self.tokenList) == 0:
+            ret += "\n  tokenList = []"
+        else:
+            ret += "\n  tokenList = [TokenList<%s>]" % str(len(self.tokenList))
+        if self.tokenTree == None:
+            ret += "\n  tokenTree = None"
+        else:
+            ret += "\n  tokenTree = [TokenTree<%s>]" % str(len(self.tokenTree))
+        ret += "\n}"
+        return ret
+
+    def parse(self):
+        if not os.path.isfile(self.inputPath):
+            self.errMsg = "Can't load file from '%s'\n" % self.inputPath
+            return
+
+        idPud = open(self.inputPath, "r").read()
+        self.tokenList = conllu.parse(idPud)
+        self.tokenTree = conllu.parse_tree(idPud)
+
+    def removeNonProjectives(self):
+        nonProjectives = []
+        for i in range(len(self.tokenList)):
+            isProjective = True
+            sentence = self.tokenList[i]
+            for j in range(len(sentence)):
+                if not isProjective:
+                    break
+
+                word = sentence[j]
+                head = word["head"] - 1
+                if head == -1:
+                    continue
+
+                left = min(j, head) + 1
+                right = max(j, head)
+                while left < right and isProjective:
+                    currHead = sentence[left]["head"]
+                    while currHead != head + 1 and currHead != 0:
+                        currHead = sentence[currHead - 1]["head"]
+                    if currHead != head + 1:
+                        isProjective = False
+                    left += 1
+            if not isProjective:
+                nonProjectives.append(i)
+
+        for i in nonProjectives:
+            del self.tokenList[i]
+            del self.tokenTree[i]
 
 def run():
     args = sys.argv
@@ -66,12 +132,7 @@ def run():
     if args[1] == "parse-pud":
         print("Parsing ID PUD...")
 
-        cd = os.path.dirname(os.path.realpath(__file__))
-        udModelPath = cd + "/../models/indonesian-gsd-ud-2.5-191206.udpipe"
-        udInputPath = cd + "/../datasets/id_pud-ud-test-revised-1-0.conllu"
-        udOutputPath = cd + "/../datasets/id_pud-ud-revised-1-0.parsed.conllu"
-
-        idPudParser = IDPUDParser(udModelPath, udInputPath, udOutputPath)
+        idPudParser = IDPUDParser(udModelPath, udInputPath, udParsedPath)
         idPudParser.proceed()
 
         if not idPudParser.parsed:
@@ -79,6 +140,12 @@ def run():
             sys.exit(1)
 
         print("ID PUD successfully parsed.")
+    elif args[1] == "build-ccgbank":
+        print("Building CCGbank...")
+
+        idConllu = CONLLU(udParsedPath)
+        idConllu.parse()
+        idConllu.removeNonProjectives()
     else:
         print("Unknown command. See \"help\" for more.")
 
